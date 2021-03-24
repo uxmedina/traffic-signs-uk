18854ed133ca8548a155c8bfe85e1bcd90c43046
uxmedina <47784579+uxmedina@users.noreply.github.com>
2021-03-23 23:18:01 -0300

    all working.

--- js/lightbox.js
+++ js/lightbox.js
@@ -16,18 +16,14 @@ jQuery(document).ready(function(){
 	$( ".teste" ).text( "There are " + n + " divs.");
 
     jQuery('.enlarge').on('click', function(){
-    var $this = $(this);
+	    var $this = $(this);
 
-    var text = $this.parent('div').siblings('.card-body').find('p').text();
-    var sign = $this.siblings().attr('src');
+	    var text = $this.parent('div').siblings('.card-body').find('p').text();
+	    var sign = $this.siblings().attr('src');
 
-    $('.sign-intro').html(text);
+	    $('.sign-intro').html(text);
 
-  	$('.sign-image').attr('src', sign);
-  	$('.sign-image').attr('alt', text);    
-
-   
-
-
+	  	$('.sign-image').attr('src', sign);
+	  	$('.sign-image').attr('alt', text);
     });
 });
